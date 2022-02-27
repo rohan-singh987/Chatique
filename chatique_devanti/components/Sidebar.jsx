b16805f1a97d245a805ca099666a38be60556804
Rohan Singh <rohannsingh98765@gmail.com>
2022-02-28 00:01:58 +0530

    sidebar
    Signed-off-by: Rohan Singh <rohannsingh98765@gmail.com>

---
+++ chatique_devanti/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../styles/Sidebar.module.css'
+const Sidebar = () => {
+  return (
+    
+
+    <div className={styles.wrapper}>
+        Sidebar
+    </div>
+  )
+}
+
+export default Sidebar
