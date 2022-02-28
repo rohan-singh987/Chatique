2a331df5b962f58847c5df6f2bed917b23150759
Rohan Singh <rohannsingh98765@gmail.com>
2022-02-28 19:59:40 +0530

    global updation
    Signed-off-by: Rohan Singh <rohannsingh98765@gmail.com>

--- chatique_devanti/pages/index.js
+++ chatique_devanti/pages/index.js
@@ -1,15 +1,21 @@
 
+// import Image  from 'next/image';
 import Sidebar from '../components/Sidebar'
 import styles from '../styles/Home.module.css'
+import ConversationList from '../components/ConversationList';
+import logo from '../assets/icons/logo.png'
+
 
 export default function Home() {
   return (
     <div className={styles.wrapper}>
       {/* Sidebar */}
+      
       <Sidebar />
 
       <div className={styles.main}>
         {/* ConversationList */}
+        <ConversationList />
         {/* ChatView */}
 
       </div>
